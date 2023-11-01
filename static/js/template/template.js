f6a442016e460693a15a20c947521cd530bfcfca
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-01 13:24:46 +0700

    add(addapi) : input api untuk kebutuhan crud

---
+++ static/js/template/template.js
@@ -0,0 +1,6 @@
+// Enpoint GET
+export let UrlGetProvinsiSekolah = "https://komarbe.ulbi.ac.id/wilayah-provinsi/get";
+export let UrlGetKotaSekolah = "https://komarbe.ulbi.ac.id/wilayah-kota/get";
+
+// Endpoint POST
+export let UrlPostDaftarMahasiswa = "https://komarbe.ulbi.ac.id/daftar"
