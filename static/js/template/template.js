4688c737a4774461da7c6644b289f1e4d39e9b1e
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-01 16:07:41 +0700

    add(getsekolah) : add endpoint get sekolah

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -1,6 +1,7 @@
 // Enpoint GET
 export let UrlGetProvinsiSekolah = "https://komarbe.ulbi.ac.id/wilayah-provinsi/get";
 export let UrlGetKotaSekolah = "https://komarbe.ulbi.ac.id/wilayah-kota/get";
+export let UrlGetSekolah = "https://komarbe.ulbi.ac.id/sekolah";
 
 // Endpoint POST
 export let UrlPostDaftarMahasiswa = "https://komarbe.ulbi.ac.id/daftar"
