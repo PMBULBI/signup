ef432d6c149742b7c87cb30512cbbecc78307152
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-04 16:47:14 +0700

    add(endpoint) : update endpoint terbaru dari backend

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -2,6 +2,9 @@
 export let UrlGetProvinsiSekolah = "https://komarbe.ulbi.ac.id/wilayah-provinsi/get";
 export let UrlGetKotaSekolah = "https://komarbe.ulbi.ac.id/wilayah-kota/get";
 export let UrlGetSekolah = "https://komarbe.ulbi.ac.id/sekolah";
+export let UrlGetNamaProvinsi = "https://komarbe.ulbi.ac.id/wilayah-provinsi/getnama";
+export let UrlGetKotaByIdProv = "https://komarbe.ulbi.ac.id/wilayah-kota/getidprov";
+export let UrlGetKotaByIdProvNmKota = "https://komarbe.ulbi.ac.id/wilayah-kota/getidprovnmkota";
 
 // Endpoint POST
 export let UrlPostDaftarMahasiswa = "https://komarbe.ulbi.ac.id/daftar"
