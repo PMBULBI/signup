cc286b11b19c7be40c17bd20eeec825836cac740
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-06 14:37:03 +0700

    add(stylejs) : add style untuk index

---
+++ static/js/template/style.js
@@ -0,0 +1,23 @@
+// Untuk Input Sekolah yang Belum Terdaftar
+const checkbox = document.getElementById("flexCheckDefault");
+// Mendapatkan elemen-elemen inputan sekolah lainnya
+const manualSchoolInput = document.getElementById("manualSchoolInput");
+const manualCityInput = document.getElementById("manualCityInput");
+const manualProvinceInput = document.getElementById("manualProvinceInput");
+const asalSekolahReadonly = document.getElementById("AsalSekolah");
+// Menambahkan event listener untuk perubahan status checkbox
+checkbox.addEventListener("change", function() {
+  if (checkbox.checked) {
+    // Jika checkbox diceklist, tampilkan inputan sekolah lainnya
+    manualSchoolInput.style.display = "block";
+    manualCityInput.style.display = "block";
+    manualProvinceInput.style.display = "block";
+    asalSekolahReadonly.readOnly = true;
+  } else {
+    // Jika checkbox tidak diceklist, sembunyikan inputan sekolah lainnya
+    manualSchoolInput.style.display = "none";
+    manualCityInput.style.display = "none";
+    manualProvinceInput.style.display = "none";
+    asalSekolahReadonly.readOnly = false;
+  }
+});
