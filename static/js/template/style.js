52a1baacaf5453089accea8d3b953e32ddef319c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-07 11:22:09 +0700

    fix(bugnohp) : input no hp angka saja & max 13 angka

--- static/js/template/style.js
+++ static/js/template/style.js
@@ -1,3 +1,17 @@
+// Inputkan untuk No Handphone/WhatsApp
+const noHandphoneInput = document.getElementById("NoHandphone");
+// Menambahkan event listener untuk memvalidasi input
+noHandphoneInput.addEventListener("input", function() {
+  // Menghapus karakter selain angka dari input
+  const sanitizedInput = noHandphoneInput.value.replace(/[^0-9]/g, '');
+  // Membatasi panjang input menjadi maksimal 13 angka
+  if (sanitizedInput.length > 13) {
+    noHandphoneInput.value = sanitizedInput.slice(0, 13);
+  } else {
+    noHandphoneInput.value = sanitizedInput;
+  }
+});
+
 // Untuk Input Sekolah yang Belum Terdaftar
 const checkbox = document.getElementById("flexCheckDefault");
 // Mendapatkan elemen-elemen inputan sekolah lainnya
